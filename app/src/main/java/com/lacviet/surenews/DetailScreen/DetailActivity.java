99aadd0828fc5a01832aacb1acbf3df80b8d48ac
nguyenhoangphuc96 <nguyenhoangphuc171196@gmail.com>
2018-06-26 17:26:53 +0700

    layout detail

---
+++ app/src/main/java/com/lacviet/surenews/DetailScreen/DetailActivity.java
@@ -0,0 +1,42 @@
+package com.lacviet.surenews.DetailScreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.lacviet.surenews.R;
+
+public class DetailActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        addControl();
+        actionBar();
+
+    }
+
+    private void addControl() {
+        toolbar.findViewById(R.id.toolbar);
+    }
+    private void actionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(R.drawable.ic_back_arrow);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+    }
+}
--- app/src/main/java/com/lacviet/surenews/MainScreenTab/TabHomeFragment.java
+++ app/src/main/java/com/lacviet/surenews/MainScreenTab/TabHomeFragment.java
@@ -1,5 +1,6 @@
 package com.lacviet.surenews.MainScreenTab;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,6 +20,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.lacviet.surenews.Adapter.HomeTabRCVAdapter;
+import com.lacviet.surenews.DetailScreen.DetailActivity;
 import com.lacviet.surenews.Model.HomeNewsModel;
 import com.lacviet.surenews.R;
 
@@ -151,6 +153,7 @@ public class TabHomeFragment extends Fragment {
 
             @Override
             public void onPostClick(long id) {
+                startDetailActivity();
 
             }
         });
@@ -179,5 +182,12 @@ public class TabHomeFragment extends Fragment {
             }
         };
         recyclerView.addOnScrollListener(scrollListener);*/
+    }
+
+    private void startDetailActivity() {
+        Intent intent = new Intent(getContext(), DetailActivity.class);
+        /*KeyString key = new KeyString();
+        intent.putExtra(key.ITEM_KEY, id);*/
+        startActivity(intent);
     }
 }
