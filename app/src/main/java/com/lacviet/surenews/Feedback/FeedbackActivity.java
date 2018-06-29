edcf34d25789a5c94a5eef01a098f5299259c63c
nguyenhoangphuc96 <nguyenhoangphuc171196@gmail.com>
2018-06-29 10:55:30 +0700

    Complete Feedback

--- app/src/main/java/com/lacviet/surenews/Feedback/FeedbackActivity.java
+++ app/src/main/java/com/lacviet/surenews/Feedback/FeedbackActivity.java
@@ -24,10 +24,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jaeger.library.StatusBarUtil;
 import com.lacviet.surenews.R;
 import com.vansuita.pickimage.BuildConfig;
 import com.vansuita.pickimage.bean.PickResult;
@@ -36,58 +38,34 @@ import com.vansuita.pickimage.dialog.PickImageDialog;
 import com.vansuita.pickimage.listeners.IPickCancel;
 import com.vansuita.pickimage.listeners.IPickResult;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-
-import static android.Manifest.permission.CAMERA;
-
-public class FeedbackActivity extends AppCompatActivity implements IPickResult {
+public class FeedbackActivity extends AppCompatActivity implements IPickResult, View.OnClickListener {
     private Toolbar toolbar;
     private TextView tvTitleToolbar;
     ImageView imv1, imv2, imv3;
+    Boolean flagImv1= false;
+    Boolean flagImv2= false;
+    Boolean flagImv3= false;
+    Button btnSend;
     //
-    Bitmap myBitmap;
-    Uri picUri;
-    private ArrayList<String> permissionsToRequest;
-    private ArrayList<String> permissionsRejected = new ArrayList<>();
-    private ArrayList<String> permissions = new ArrayList<>();
-
-    private final static int ALL_PERMISSIONS_RESULT = 107;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        //set transparent stt bar
+        StatusBarUtil.setTransparent(this);
         addControl();
         actionBar();
         addEvent();
-        /*permissions.add(CAMERA);
-        permissionsToRequest = findUnAskedPermissions(permissions);
-        //get the permissions we have asked for before but are not granted..
-        //we will store this in a global list to access later.
 
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-
-            if (permissionsToRequest.size() > 0)
-                requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]), ALL_PERMISSIONS_RESULT);
-        }*/
     }
 
     private void addEvent() {
-        imv3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PickImageDialog.build(new PickSetup()).show(FeedbackActivity.this);
-            }
-        });
+        imv1.setOnClickListener(this);
+        imv2.setOnClickListener(this);
+        imv3.setOnClickListener(this);
+        btnSend.setOnClickListener(this);
 
     }
 
@@ -113,276 +91,62 @@ public class FeedbackActivity extends AppCompatActivity implements IPickResult {
         imv1 = findViewById(R.id.imvFeedback1);
         imv2 = findViewById(R.id.imvFeedback2);
         imv3 = findViewById(R.id.imvFeedback3);
+        btnSend = findViewById(R.id.btnSendFeedback);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.imvFeedback3:{
+                PickImageDialog.build(new PickSetup()).show(FeedbackActivity.this);
+                flagImv3 = true;
+
+                break;
+            }
+            case R.id.imvFeedback2:{
+                PickImageDialog.build(new PickSetup()).show(FeedbackActivity.this);
+                flagImv2 = true;
+
+                break;
+            }
+            case R.id.imvFeedback1:{
+                PickImageDialog.build(new PickSetup()).show(FeedbackActivity.this);
+                flagImv1 = true;
+                break;
+            }
+            case R.id.btnSendFeedback:{
+                startConfirmSDTActivity();
+
+            }
+            default:break;
+        }
+    }
+
+    private void startConfirmSDTActivity() {
+        Intent intent = new Intent(FeedbackActivity.this,SDTConfirmActivity.class);
+        startActivity(intent);
     }
 
     @Override
     public void onPickResult(PickResult pickResult) {
-        imv3.setImageBitmap(pickResult.getBitmap());
-
-        imv2.setVisibility(View.VISIBLE);
-    }
-    /**
-     * Create a chooser intent to select the source to get image from.<br/>
-     * The source can be camera's (ACTION_IMAGE_CAPTURE) or gallery's (ACTION_GET_CONTENT).<br/>
-     * All possible sources are added to the intent chooser.
-     */
-   /* public Intent getPickImageChooserIntent() {
-
-        // Determine Uri of camera image to save.
-        Uri outputFileUri = getCaptureImageOutputUri();
-
-        List<Intent> allIntents = new ArrayList<>();
-        PackageManager packageManager = getPackageManager();
-
-        // collect all camera intents
-        Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
-        for (ResolveInfo res : listCam) {
-            Intent intent = new Intent(captureIntent);
-            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
-            intent.setPackage(res.activityInfo.packageName);
-            if (outputFileUri != null) {
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-            }
-            allIntents.add(intent);
+        if(flagImv3)
+        {
+            imv3.setImageBitmap(pickResult.getBitmap());
+            imv2.setVisibility(View.VISIBLE);
+            flagImv3=false;
         }
-
-        // collect all gallery intents
-        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        galleryIntent.setType("image/*");
-        List<ResolveInfo> listGallery = packageManager.queryIntentActivities(galleryIntent, 0);
-        for (ResolveInfo res : listGallery) {
-            Intent intent = new Intent(galleryIntent);
-            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
-            intent.setPackage(res.activityInfo.packageName);
-            allIntents.add(intent);
+        if (flagImv2){
+            imv2.setImageBitmap(pickResult.getBitmap());
+            imv1.setVisibility(View.VISIBLE);
+            flagImv2=false;
         }
-
-        // the main intent is the last in the list (fucking android) so pickup the useless one
-        Intent mainIntent = allIntents.get(allIntents.size() - 1);
-        for (Intent intent : allIntents) {
-            if (intent.getComponent().getClassName().equals("com.android.documentsui.DocumentsActivity")) {
-                mainIntent = intent;
-                break;
-            }
-        }
-        allIntents.remove(mainIntent);
-
-        // Create a chooser from the main intent
-        Intent chooserIntent = Intent.createChooser(mainIntent, "Select source");
-
-        // Add all other intents
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, allIntents.toArray(new Parcelable[allIntents.size()]));
-
-        return chooserIntent;
-    }
-
-
-    *//**
-     * Get URI to image received from capture by camera.
-     *//*
-    private Uri getCaptureImageOutputUri() {
-        Uri outputFileUri = null;
-        File getImage = getExternalCacheDir();
-        if (getImage != null) {
-            outputFileUri = Uri.fromFile(new File(getImage.getPath(), "profile.png"));
-        }
-        return outputFileUri;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        Bitmap bitmap;
-        if (resultCode == Activity.RESULT_OK) {
-
-
-
-            if (getPickImageResultUri(data) != null) {
-                picUri = getPickImageResultUri(data);
-
-                try {
-                    myBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), picUri);
-                    myBitmap = rotateImageIfRequired(myBitmap, picUri);
-                    myBitmap = getResizedBitmap(myBitmap, 500);
-
-                    imv3.setImageBitmap(myBitmap);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-            } else {
-
-
-                bitmap = (Bitmap) data.getExtras().get("data");
-
-                myBitmap = bitmap;
-
-
-                imv3.setImageBitmap(myBitmap);
-
-            }
-
+        if (flagImv1)
+        {
+            imv1.setImageBitmap(pickResult.getBitmap());
+            flagImv1=false;
         }
 
     }
-
-    private static Bitmap rotateImageIfRequired(Bitmap img, Uri selectedImage) throws IOException {
-
-        ExifInterface ei = new ExifInterface(selectedImage.getPath());
-        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-
-        switch (orientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                return rotateImage(img, 90);
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                return rotateImage(img, 180);
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                return rotateImage(img, 270);
-            default:
-                return img;
-        }
-    }
-
-    private static Bitmap rotateImage(Bitmap img, int degree) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(degree);
-        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
-        img.recycle();
-        return rotatedImg;
-    }
-
-    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-
-        float bitmapRatio = (float) width / (float) height;
-        if (bitmapRatio > 0) {
-            width = maxSize;
-            height = (int) (width / bitmapRatio);
-        } else {
-            height = maxSize;
-            width = (int) (height * bitmapRatio);
-        }
-        return Bitmap.createScaledBitmap(image, width, height, true);
-    }
-
-
-    *//**
-     * Get the URI of the selected image from { #getPickImageChooserIntent()}.<br/>
-     * Will return the correct URI for camera and gallery image.
-     *
-     * param data the returned data of the activity result
-     *//*
-    public Uri getPickImageResultUri(Intent data) {
-        boolean isCamera = true;
-        if (data != null) {
-            String action = data.getAction();
-            isCamera = action != null && action.equals(MediaStore.ACTION_IMAGE_CAPTURE);
-        }
-
-
-        return isCamera ? getCaptureImageOutputUri() : data.getData();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        // save file url in bundle as it will be null on scren orientation
-        // changes
-        outState.putParcelable("pic_uri", picUri);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-        // get the file url
-        picUri = savedInstanceState.getParcelable("pic_uri");
-    }
-
-    private ArrayList<String> findUnAskedPermissions(ArrayList<String> wanted) {
-        ArrayList<String> result = new ArrayList<String>();
-
-        for (String perm : wanted) {
-            if (!hasPermission(perm)) {
-                result.add(perm);
-            }
-        }
-
-        return result;
-    }
-
-    private boolean hasPermission(String permission) {
-        if (canMakeSmores()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
-            }
-        }
-        return true;
-    }
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(this)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
-    }
-
-    private boolean canMakeSmores() {
-        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-
-        switch (requestCode) {
-
-            case ALL_PERMISSIONS_RESULT:
-                for (String perms : permissionsToRequest) {
-                    if (hasPermission(perms)) {
-
-                    } else {
-
-                        permissionsRejected.add(perms);
-                    }
-                }
-
-                if (permissionsRejected.size() > 0) {
-
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
-                            showMessageOKCancel("These permissions are mandatory for the application. Please allow access.",
-                                    new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-                                                //Log.d("API123", "permisionrejected " + permissionsRejected.size());
-
-                                                requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
-                                            }
-                                        }
-                                    });
-                            return;
-                        }
-                    }
-
-                }
-
-                break;
-        }
-
-    }*/
-
-
-
 }
---
+++ app/src/main/java/com/lacviet/surenews/Feedback/SDTConfirmActivity.java
@@ -0,0 +1,96 @@
+package com.lacviet.surenews.Feedback;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.jaeger.library.StatusBarUtil;
+import com.lacviet.surenews.MainActivity;
+import com.lacviet.surenews.R;
+
+public class SDTConfirmActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TextView tvTitleToolbar;
+    Button btnConfirm;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdt_confirm);
+        //set transparent stt bar
+        StatusBarUtil.setTransparent(this);
+        addControl();
+        actionBar();
+        addEvent();
+    }
+
+    private void addEvent() {
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogSuccess();
+            }
+        });
+
+    }
+
+    private void showDialogSuccess() {
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Gửi góp ý thành công!");
+        alertDialogBuilder
+                .setMessage("Bạn có muốn gửi tiếp góp ý?")
+                .setCancelable(false)
+                .setPositiveButton("Có",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                            }
+                        })
+
+                .setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startMainActivity();
+                        //dialog.cancel();
+                    }
+                })
+        .setIcon(R.drawable.ic_success);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(SDTConfirmActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void actionBar() {
+        setSupportActionBar(toolbar);
+        tvTitleToolbar.setText(toolbar.getTitle());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(R.drawable.ic_back_arrow);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void addControl() {
+        toolbar = findViewById(R.id.toolbar);
+        tvTitleToolbar = findViewById(R.id.tvTitleToolbar);
+        btnConfirm = findViewById(R.id.btnConfirmSDT);
+
+
+    }
+}
